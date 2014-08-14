d7de39ccd9028ed7c4616864fcce70eb355227eb
mirkotp <mirkotp@users.noreply.github.com>
2014-08-14 17:03:15 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -7,9 +7,9 @@
    	 * though it needs to be enabled from user's configuration.
    	 */
 	
-	$config['host'] 		= "10.10.5.113";
-	$config['port']	 		= "8080";
+	$config['host'] 	= "10.10.5.113";
+	$config['port']	 	= "8080";
 	$config['webapp_name']	= "queuemetrics";
-	$config['username']		= "robot";
+	$config['username']	= "robot";
 	$config['password'] 	= "robot";
 ?>
